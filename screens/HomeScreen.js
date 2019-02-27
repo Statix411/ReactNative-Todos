cd71d43f76872970ce7d1de0a6e5d59e2e7a5d07
Statix411 <statix411@gmail.com>
2019-02-27 14:29:06 +0300

    init commint

---
+++ screens/HomeScreen.js
@@ -0,0 +1,212 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  state = {
+    todos: [],
+    inputed: ""
+  };
+
+  addtodo = () => {
+
+    if(this.state.inputed.trim().length > 0)
+      this.setState({
+        todos: this.state.todos.concat(this.state.inputed)
+      });
+  }
+
+  delTodo = (index) => {
+    let todos = this.state.todos
+    todos.splice(index, 1)
+    this.setState({
+      todos: todos
+    });
+  }
+
+  render() {
+    let todos = this.state.todos.map((todo) => todo)
+    return (
+      <View style={styles.container}>
+      <View style={styles.paddingInput}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Add"
+          onChangeText={(text) => this.setState({inputed: text})}
+          onSubmitEditing={this.addtodo}
+         />
+      </View>
+        <View style={styles.container}>
+         <FlatList
+           data={todos}
+           renderItem={({item, index}) =>
+
+           <View>
+            <Text style={styles.item}>{item} </Text>
+            <Button title="x" onPress={() => this.delTodo(index)}/>
+           </View>
+         }
+         />
+
+       </View>
+
+
+      </View>
+    );
+  }
+
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      );
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful development
+          tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  ListTodos:{
+    backgroundColor: '#ffae00',
+  },
+  container: {
+   flex: 1,
+   paddingTop: 10
+  },
+  paddingInput:{
+    paddingTop: 20,
+    paddingLeft: 150,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 1,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
